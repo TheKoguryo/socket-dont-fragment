05e9d6cdefd3805195379b444e9930b69b9bfe5c
TheKoguryo <thekooguryo@gmail.com>
2024-06-10 13:34:09 +0900

    C#

--- C#/Client/Program.cs
+++ C#/Client/Program.cs
@@ -13,104 +13,104 @@ class Program {
 // Main Method
 static void Main(string[] args)
 {
-	ExecuteClient(args);
+    ExecuteClient(args);
 }
 
 // ExecuteClient() Method
 static void ExecuteClient(string[] args)
 {
-	string SERVER = "150.xxx.xxx.xx";
-	int PORT = 8080;
-	int BUFFER_SIZE = 1024;
-	int MSG_SIZE = 6000;
-	bool DONT_FRAGMENT = true;
+    string SERVER = "150.xxx.xxx.xx";
+    int PORT = 8080;
+    int BUFFER_SIZE = 1024;
+    int MSG_SIZE = 6000;
+    bool DONT_FRAGMENT = true;
 
-	if (args.Length >= 1) {
-		SERVER = args[0];
-	}
-	if (args.Length >= 2) {
-		PORT = Int32.Parse(args[1]);
-	}
-	if (args.Length >= 3) {
-		DONT_FRAGMENT = Boolean.Parse(args[2]);
-	}	
+    if (args.Length >= 1) {
+        SERVER = args[0];
+    }
+    if (args.Length >= 2) {
+        PORT = Int32.Parse(args[1]);
+    }
+    if (args.Length >= 3) {
+        DONT_FRAGMENT = Boolean.Parse(args[2]);
+    }	
 
-	try {
-		
-		// Establish the remote endpoint for the socket.
-		IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+    try {
+        
+        // Establish the remote endpoint for the socket.
+        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
         IPAddress ipAddr = System.Net.IPAddress.Parse(SERVER);
-		IPEndPoint localEndPoint = new IPEndPoint(ipAddr, PORT);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, PORT);
 
-		// Creation TCP/IP Socket using 
-		// Socket Class Constructor
-		Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        // Creation TCP/IP Socket using 
+        // Socket Class Constructor
+        Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-		try {
-			
-			// Connect Socket to the remote 
-			// endpoint using method Connect()
-			sender.Connect(localEndPoint);
+        try {
+            
+            // Connect Socket to the remote 
+            // endpoint using method Connect()
+            sender.Connect(localEndPoint);
 
             // DontFragment
             sender.DontFragment = DONT_FRAGMENT;
-			if (sender.DontFragment) {
-				Console.WriteLine("DontFragment(DF) is enabled");
-			}
+            if (sender.DontFragment) {
+                Console.WriteLine("DontFragment(DF) is enabled");
+            }
             //sender.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.DontFragment, 1);
 
-			// We print EndPoint information 
-			// that we are connected
-			Console.WriteLine("Socket connected to {0} ", sender.RemoteEndPoint.ToString());
+            // We print EndPoint information 
+            // that we are connected
+            Console.WriteLine("Socket connected to {0} ", sender.RemoteEndPoint.ToString());
 
-			// Creation of message that
-			// we will send to Server
-			//byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+            // Creation of message that
+            // we will send to Server
+            //byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
 
-			string message = MSG_SIZE.ToString();
-			string paddedMessage = message.PadRight(MSG_SIZE - "<EOF>".Length, '_');
+            string message = MSG_SIZE.ToString();
+            string paddedMessage = message.PadRight(MSG_SIZE - "<EOF>".Length, '_');
 
-			byte[] messageSent = Encoding.ASCII.GetBytes(paddedMessage + "<EOF>");
-			int byteSent = sender.Send(messageSent);
+            byte[] messageSent = Encoding.ASCII.GetBytes(paddedMessage + "<EOF>");
+            int byteSent = sender.Send(messageSent);
 
-			Console.WriteLine("Sent: {0} bytes", byteSent);
-			Console.WriteLine(Encoding.Default.GetString(messageSent));
+            Console.WriteLine("Sent: {0} bytes", byteSent);
+            Console.WriteLine(Encoding.Default.GetString(messageSent));
 
-			// Data buffer
-			byte[] messageReceived = new byte[BUFFER_SIZE];
+            // Data buffer
+            byte[] messageReceived = new byte[BUFFER_SIZE];
 
-			// We receive the message using 
-			// the method Receive(). This 
-			// method returns number of bytes
-			// received, that we'll use to 
-			// convert them to string
-			int byteRecv = sender.Receive(messageReceived);
-			Console.WriteLine("Message from Server: {0}", 
-				Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+            // We receive the message using 
+            // the method Receive(). This 
+            // method returns number of bytes
+            // received, that we'll use to 
+            // convert them to string
+            int byteRecv = sender.Receive(messageReceived);
+            Console.WriteLine("Message from Server: {0}", 
+                Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
 
-			// Close Socket using 
-			// the method Close()
-			sender.Shutdown(SocketShutdown.Both);
-			sender.Close();
-		}
-		
-		// Manage of Socket's Exceptions
-		catch (ArgumentNullException ane) {
-			Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-		}
-		
-		catch (SocketException se) {
-			Console.WriteLine("SocketException : {0}", se.ToString());
-		}
-		
-		catch (Exception e) {
-			Console.WriteLine("Unexpected exception : {0}", e.ToString());
-		}
-	}
-	
-	catch (Exception e) {
-		Console.WriteLine(e.ToString());
-	}
+            // Close Socket using 
+            // the method Close()
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        
+        // Manage of Socket's Exceptions
+        catch (ArgumentNullException ane) {
+            Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+        }
+        
+        catch (SocketException se) {
+            Console.WriteLine("SocketException : {0}", se.ToString());
+        }
+        
+        catch (Exception e) {
+            Console.WriteLine("Unexpected exception : {0}", e.ToString());
+        }
+    }
+
+    catch (Exception e) {
+        Console.WriteLine(e.ToString());
+    }
 }
 
 }
--- C#/Server/Program.cs
+++ C#/Server/Program.cs
@@ -18,27 +18,27 @@ static void Main(string[] args)
  
 public static void ExecuteServer(string[] args)
 {
-	int PORT = 8080;
-	int BUFFER_SIZE = 1024;
-	bool DONT_FRAGMENT = false;
+    int PORT = 8080;
+    int BUFFER_SIZE = 1024;
+    bool DONT_FRAGMENT = false;
 
-	if (args.Length >= 1) {
-		PORT = Int32.Parse(args[0]);
-	}
-	if (args.Length >= 2) {
-		DONT_FRAGMENT = Boolean.Parse(args[1]);
-	}	    
+    if (args.Length >= 1) {
+        PORT = Int32.Parse(args[0]);
+    }
+    if (args.Length >= 2) {
+        DONT_FRAGMENT = Boolean.Parse(args[1]);
+    }	    
 
     // Establish the local endpoint for the socket.
     // Dns.GetHostName returns the name of the host running the application.
     IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
     IPAddress ipAddr = ipHost.AddressList[1];
     IPEndPoint localEndPoint = new IPEndPoint(ipAddr, PORT);
- 
+
     // Creation TCP/IP Socket using 
     // Socket Class Constructor
     Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
- 
+
     try {
         // Using Bind() method we associate a
         // network address to the Server Socket
@@ -46,16 +46,16 @@ public static void ExecuteServer(string[] args)
         // Server Socket must know this network
         // Address
         listener.Bind(localEndPoint);
- 
+
         // Using Listen() method we create 
         // the Client list that will want
         // to connect to Server
         listener.Listen(10);
- 
+
         while (true) {
 
             Console.WriteLine("Listening on " + PORT);
- 
+
             // Suspend while waiting for
             // incoming connection Using 
             // Accept() method the server 
@@ -64,18 +64,18 @@ public static void ExecuteServer(string[] args)
 
             // DontFragment
             clientSocket.DontFragment = DONT_FRAGMENT;
-			if (clientSocket.DontFragment) {
-				Console.WriteLine("DontFragment(DF) is enabled");
-			}
- 
+            if (clientSocket.DontFragment) {
+                Console.WriteLine("DontFragment(DF) is enabled");
+            }
+
             // Data buffer
             byte[] bytes = new Byte[BUFFER_SIZE];
             string data = "";
- 
+
             while (true) {
- 
+
                 int numByte = clientSocket.Receive(bytes);
-                 
+                    
                 data += Encoding.ASCII.GetString(bytes, 0, numByte);
                                             
                 if (data.IndexOf("<EOF>") > -1)
@@ -84,13 +84,13 @@ public static void ExecuteServer(string[] args)
 
             Console.WriteLine("[{0}] Received: {1} ", clientSocket.RemoteEndPoint, data);
             byte[] message = Encoding.ASCII.GetBytes(Encoding.ASCII.GetBytes(data).Length + " bytes received");            
- 
+
             // Send a message to Client 
             // using Send() method
             clientSocket.Send(message);
 
             Console.WriteLine("[{0}] Sent: {1} ", clientSocket.RemoteEndPoint, Encoding.ASCII.GetString(message));
- 
+
             // Close client Socket using the
             // Close() method. After closing,
             // we can use the closed Socket 
@@ -101,7 +101,7 @@ public static void ExecuteServer(string[] args)
             Console.WriteLine("");
         }
     }
-     
+        
     catch (Exception e) {
         Console.WriteLine(e.ToString());
     }
